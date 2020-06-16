cf8943dfd083aa16a969d6adccf6df768ae6c766
Şeyda Arı <ariseydaari@gmail.com>
2020-06-16 22:38:46 +0300

    Build and run
    Build and run program given below. Examine the output.

---
+++ plusorminus.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+
+// Build and run program given below. Examine the output. 
+
+int main(void) {  
+
+
+int a = 5, b, c, d, e; 
+
+
+ b = a++; 
+ c = ++a; 
+ d = a--;  
+   
+   
+   printf("a=%d\nb=%d\nc=%d\nd=%d\ne=%d\n", a, b, c, d, e);
+   
+   
+   
+   
+   
+    return 0; 
+	 
+	 
+	 }   
